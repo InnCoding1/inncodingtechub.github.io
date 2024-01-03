3b009361bc1796db2049d4f44a20f9ffcc9bbd8c
InnCoding1 <inncoding1@gmail.com>
2024-01-03 21:17:01 +0100

    Modified all the code

---
+++ contactus.js
@@ -0,0 +1,15 @@
+//<form name="submit-to-google-sheet">
+ // <input name="email" type="email" placeholder="Email" required>
+ // <button type="submit">Send</button>
+//</input></form>
+
+ /* const scriptURL = 'https://script.google.com/macros/s/AKfycbwAZZvKOZvno7gowbw61j3CgWC1AlWZFhoveAY8c6QugZqawSSvF9Zko9IYoRNKoNe9Xg/exec';
+
+  const form = document.forms['connect-form-to-google-sheet']
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error!', error.message))
+  });*/
